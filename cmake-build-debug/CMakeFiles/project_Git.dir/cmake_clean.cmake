c31a795cc00f7982c7d705ce6d5ae46a64b941b3
LAPTOP-F1REOL2V\LEGION <235969@edu.p.lodz.pl>
2021-02-17 10:24:09 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_Git.dir/main.cpp.obj"
+  "libproject_Git.dll.a"
+  "project_Git.exe"
+  "project_Git.exe.manifest"
+  "project_Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
